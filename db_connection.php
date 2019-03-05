13522fca2f7248923f33ffc841d5e095983540c8
Mukhtor <mukhtorbabadjanov@gmail.com>
2019-03-05 22:22:01 +0500

    contactga dizayn boyicha

---
+++ db_connection.php
@@ -0,0 +1,25 @@
+<?php
+
+$dsn = 'mysql:host=localhost;dbname=project_contact';
+
+$db_user = 'root';
+
+$db_password = '';
+
+$connection = new PDO($dsn, $db_user, $db_password);
+
+//$query = $connection->query(
+
+//    'INSERT INTO contacts (first_name, last_name, phone) VALUES ("John", "Doe", "+87987989987")'
+
+//);
+
+//$query = $connection->prepare(
+
+//    'INSERT INTO contacts (first_name, last_name, phone) VALUES (?, ?, ?)'
+
+//);
+
+//
+
+//$query->execute(['Maxmud', 'Shokirov', '+97979797797']);
